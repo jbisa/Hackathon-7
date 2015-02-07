f7a2e9a7a2a54e4d8eb3bfbf04caa7d393b021bd
Jay Bisa <jaybisa@Jay-Bisas-MBP.home>
2015-02-07 16:05:48 -0500

    Camera not automatically started.

--- Hackathon 7/ViewController.swift
+++ Hackathon 7/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import CoreData
+import MobileCoreServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet weak var cameraView: UIImageView!
+    
     override func viewDidLoad() {
+        sleep(1)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func takePhoto(sender: AnyObject) {
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+            var picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            var mediaTypes: Array<AnyObject> = [kUTTypeImage]
+            picker.mediaTypes = mediaTypes
+            picker.allowsEditing = false
+            self.presentViewController(picker, animated: true, completion: nil)
+        } else {
+            NSLog("No Camera")
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: NSDictionary!) {
+        NSLog("Did Finish Picking")
+        
+        
+        if(picker.sourceType == UIImagePickerControllerSourceType.Camera) {
+            var imageToSave: UIImage = info.objectForKey(UIImagePickerControllerOriginalImage) as UIImage
+            UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil)
+            
+            self.savedImageAlert()
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    func savedImageAlert() {
+        var alert:UIAlertView = UIAlertView()
+        alert.title = "Saved!"
+        alert.message = "Your picture was saved to Camera Roll"
+        alert.delegate = self
+        alert.addButtonWithTitle("Ok")
+        alert.show()
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
 }
+
+
 
